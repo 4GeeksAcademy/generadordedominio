3e5ab606b930df6d9066d0a098e6251163d15770
francoaguilar622 <123565045+francoaguilar622@users.noreply.github.com>
2025-01-10 00:17:39 +0000

    dominio

--- src/app.js
+++ src/app.js
@@ -6,6 +6,14 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+
+  Math.floor(Math.random() * pronoun.length);
+  Math.floor(Math.random() * adj.length);
+  Math.floor(Math.random() * noun.length);
+
+  document.querySelector("dominio").innerHTML =
+    pronoun[rdm1] + adj[rmd2] + noun[rmd3];
 };
